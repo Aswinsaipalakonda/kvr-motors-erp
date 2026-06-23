1e42353f3d5de733d7fab3751d316c61c7fcbfde
Aswinsaipalakonda <aswinsaipalakonda@gmail.com>
2026-06-23 16:26:45 +0530

    Auto-commit: ERP codebase updates at 2026-06-23 16:26:44

--- dashboards/app/services/api.ts
+++ dashboards/app/services/api.ts
@@ -8,8 +8,6 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
     "Cache-Control": "no-cache, no-store, must-revalidate",
-    "Pragma": "no-cache",
-    "Expires": "0",
   },
 });
 
